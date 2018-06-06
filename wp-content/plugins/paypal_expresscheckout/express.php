d29e747dce5c714878677f52c2250911a6c57446
echizenyayota <virtualmisso30@gmail.com>
2018-06-06 17:37:51 +0900

    express_admin.phpの削除

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -76,5 +76,3 @@ function paypalexpresscheckout_add_admin_menu(){
     add_submenu_page('plugins.php','PayPal Express Checkoutの設定','PayPal Express Checkoutの設定', 'administrator', __FILE__, 'paypalexpresscheckout_admin_menu');
 }
 add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
-
-require_once(__DIR__ . '/express_admin.php');
