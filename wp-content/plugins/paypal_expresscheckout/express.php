8a15feac5f2d69c235622d0fd5df67d285322d46
echizenyayota <virtualmisso30@gmail.com>
2018-06-06 17:34:46 +0900

    JSファイルをPHPに取り込む

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -16,13 +16,57 @@ Text Domain: paypal_expresscheckout
 // Client Side Restの読み込み
 function paypal_scripts() {
 	wp_enqueue_script( 'paypal-checkout', 'https://www.paypalobjects.com/api/checkout.js' );
-	wp_enqueue_script( 'paypal-expresscheckout', plugin_dir_url( __FILE__ ) . '/js/expresscheckout.js', array( 'paypal-checkout' ) );
+	// wp_enqueue_script( 'paypal-expresscheckout', plugin_dir_url( __FILE__ ) . '/js/expresscheckout.js', array( 'paypal-checkout' ) );
 }
 add_action( 'wp_enqueue_scripts', 'paypal_scripts' );
 
 // ショートコード記述によるPayPalボタンの表示
-function paypaldiv_func(){
-  $paypaldiv = '<div id="paypal-button-container"></div>';
+function paypaldiv_func( $atts ){
+	$config = shortcode_atts( array(
+		'id' => '',	// id名と第二引数が合致すれば良い
+		'price' => '0',
+		'currency' => '',
+		'env' => 'sandbox',	// 管理画面上入力
+		'color' => 'blue',
+		'size' => 'small',
+	), $atts );
+
+	if ( !$config['id'] || $config['price'] === '0' || !$config['currency'] ) return;
+
+	if ( $config['env'] === 'sandbox' ) {
+		$token = "sandbox: 'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R'";
+	} elseif ( $config['env'] === 'production' ) {
+		$token = "production: 'input your production token'";
+	}
+
+	$paypaldiv = '<div id="' . $config['id'] . '"></div>';
+	$paypaldiv .= "<script>
+		paypal.Button.render({
+			env: '$config[env]',
+			client: {
+				{$token},
+			},
+			style: {
+				color: '$config[color]',
+				size: '$config[size]',
+			},
+			commit: true,
+			payment: function(data, actions) {
+				return actions.payment.create({
+					payment: {
+						transactions: [{
+							amount: { total: '$config[price]', currency: '$config[currency]' }
+						}]
+					}
+				});
+			},
+			onAuthorize: function(data, actions) {
+				return actions.payment.execute().then(function() {
+					window.alert('Payment Complete!');
+				});
+			}
+		}, '#$config[id]');
+	</script>";
   return $paypaldiv;
 }
 add_shortcode( 'paypaldiv', 'paypaldiv_func' );
