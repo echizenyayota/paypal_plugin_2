0b7eefb5179b89cbb39fdac75724e12ad6f7071d
echizenyayota <virtualmisso30@gmail.com>
2018-06-07 14:20:46 +0900

    サブメニュープラグイン設定の表示

--- wp-content/plugins/paypal_expresscheckout/express.php
+++ wp-content/plugins/paypal_expresscheckout/express.php
@@ -18,3 +18,11 @@ function paypal_scripts() {
   wp_enqueue_script( 'paypal-checkout', 'https://www.paypalobjects.com/api/checkout.js' );
 }
 add_action( 'wp_enqueue_scripts', 'paypal_scripts' );
+
+
+
+// サブメニュープラグイン設定の表示
+function paypalexpresscheckout_add_admin_menu(){
+    add_submenu_page('plugins.php','PayPal Express Checkoutの設定','PayPal Express Checkoutの設定', 'administrator', __FILE__, 'paypalexpresscheckout_admin_menu');
+}
+add_action('admin_menu', 'paypalexpresscheckout_add_admin_menu');
